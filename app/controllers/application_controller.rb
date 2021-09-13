f3e3fa34dd86c652d3fcc3200d6131a428ede313
Joe Renteria <joe@electronflight.com>
2021-09-13 18:22:39 -0500

    commit tree da4b54555bbc732eec233c4e7ea93a9811e2e9df

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,26 @@
+require 'pry'
+
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
+
+  patch '/reviews/:id' do
+    review = Review.find(params[:id])
+    review.update(
+      score: params[:score],
+      comment: params[:comment]
+    )
+    review.to_json
+  end
+
+  post '/reviews' do
+    review = Review.create(
+      score: params[:score],
+      comment: params[:comment],
+      game_id: params[:game_id],
+      user_id: params[:user_id]
+    )
+    review.to_json
+  end
 
   get '/games' do
     games = Game.all.order(:title).limit(10)
@@ -15,5 +36,26 @@ class ApplicationController < Sinatra::Base
       } }
     })
   end
+
+  get '/reviews'do
+    reviews = Review.all
+    reviews.to_json
+  end
+
+  get '/reviews/:id' do
+    review = Review.find(params[:id])
+    review.to_json
+  end
+
+  delete '/reviews/:id' do
+    #find review with id - 1 Ullam sint enim repellendus.
+    review = Review.find(params[:id])
+    #delete review
+    review.destroy
+    #send response with deleted review
+    review.to_json
+  end
+
+  
 
 end
